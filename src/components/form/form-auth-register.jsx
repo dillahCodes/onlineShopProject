e6867c58c1da72a75f083e40c5c12e8fce231c71
abdillah25 <abijuniansayh@gmail.com>
2024-04-04 02:20:54 +0700

    "feat: add register features"

--- src/components/form/form-auth-register.jsx
+++ src/components/form/form-auth-register.jsx
@@ -3,20 +3,31 @@ import { useState } from "react";
 import ButtonComponent from "../ui/button-component";
 import FormHelpDropdown from "./form-help-dropdown";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const FormAuthRegister = () => {
+const FormAuthRegister = ({ handleInputChange, handleLoginSubmit, registerData }) => {
   const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className="w-full">
       <h1 className="mb-5 text-2xl font-bold capitalize font-space-grotesk">register</h1>
-      <form action="" className="flex flex-col w-full gap-y-5">
+      <form action="" onSubmit={handleLoginSubmit} className="flex flex-col w-full gap-y-5">
         {/* name */}
         <div className="w-full">
           <label htmlFor="name" className="capitalize">
             Name: <span className="text-red-600">*</span>
           </label>
 
-          <Input id="name" placeholder="jhon doe" type="text" size="large" className="mt-1" />
+          <Input
+            id="name"
+            name="nama"
+            placeholder="jhon doe"
+            type="text"
+            size="large"
+            className="mt-1"
+            defaultValue={registerData.nama}
+            onChange={handleInputChange}
+          />
         </div>
         {/* email */}
         <div className="w-full">
@@ -24,7 +35,16 @@ const FormAuthRegister = () => {
             email: <span className="text-red-600">*</span>
           </label>
 
-          <Input id="email" type="email" placeholder="example@gmail.com" size="large" className="mt-1" />
+          <Input
+            id="email"
+            name="email"
+            type="email"
+            placeholder="example@gmail.com"
+            size="large"
+            className="mt-1"
+            defaultValue={registerData.email}
+            onChange={handleInputChange}
+          />
         </div>
 
         {/* password */}
@@ -33,9 +53,12 @@ const FormAuthRegister = () => {
             password: <span className="text-red-600">*</span>
           </label>
           <Input.Password
+            name="password"
             className="mt-1"
             size="large"
             id="password"
+            defaultValue={registerData.password}
+            onChange={handleInputChange}
             placeholder="****"
             visibilityToggle={{
               visible: showPassword,
@@ -44,7 +67,7 @@ const FormAuthRegister = () => {
           />
         </div>
         {/* submit button */}
-        <ButtonComponent className={"w-full"} type="primary" size="large">
+        <ButtonComponent htmlType="submit" className={"w-full"} type="primary" size="large">
           <span className="capitalize">Log in</span>
         </ButtonComponent>
 
@@ -61,3 +84,9 @@ const FormAuthRegister = () => {
 };
 
 export default FormAuthRegister;
+
+FormAuthRegister.propTypes = {
+  handleInputChange: PropTypes.func.isRequired,
+  handleLoginSubmit: PropTypes.func.isRequired,
+  registerData: PropTypes.object.isRequired,
+};
--- src/pages/register-page/register-page.jsx
+++ src/pages/register-page/register-page.jsx
@@ -1,11 +1,37 @@
 import { Layout } from "antd";
 import FormAuthRegister from "../../components/form/form-auth-register";
+import { useState } from "react";
+import axios from "axios";
 
 const RegisterPage = () => {
+  const [registerData, setRegisterData] = useState({
+    nama: "",
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setRegisterData({ ...registerData, [name]: value });
+  };
+
+  const handleLoginSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(import.meta.env.VITE_API_URL + "users/register", registerData);
+      console.log("Login Successful:", response.data);
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
   return (
     <Layout className="flex items-center justify-center h-screen p-5">
       <div className="w-full md:w-[50%] lg:w-[30%] mx-auto border-[2px] border-black p-5 rounded-md">
-        <FormAuthRegister />
+        <FormAuthRegister
+          handleInputChange={handleInputChange}
+          registerData={registerData}
+          handleLoginSubmit={handleLoginSubmit}
+        />
       </div>
     </Layout>
   );
